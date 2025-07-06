afcaacc9eff2e9e4fc6c02f385798e4fea5c7490
front_academy <dmitrypetrakevich@gmail.com>
2025-07-06 11:26:30 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<PageHeader />
+
+<AppHome />
+
+
+</template>
+
+<script>
+import SvgLogo from '@/components/icons/SvgLogo.vue';
+import PageHeader from '@/components/PageHeader.vue';
+import AppHome from '@/components/AppHome.vue';
+
+
+export default {
+  components: {
+    AppHome,
+    PageHeader,
+    SvgLogo,
+
+  },
+}
+
+</script>
+
+<style lang="less">
+
+
+
+
+@keyframes flicker {
+  from {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+
+</style>
